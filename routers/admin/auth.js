1a709cf534732d800736edf280ccc0f54a41d990
vikramchdry <ec1029231058@gmail.com>
2021-04-29 03:54:10 +0200

    add admin endpoints

---
+++ routers/admin/auth.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { signup, signin } = require('../../controller/admin/auth');
+const router = express.Router();
+
+
+
+
+
+
+router.post('/admin/signin', signin);
+
+router.post('/admin/signup', signup);
+
+module.exports = router;
