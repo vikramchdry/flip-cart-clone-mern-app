05bd88c27bd539f91cb911a9ce2d5412544cf7bc
vikramchdry <ec1029231058@gmail.com>
2021-04-29 22:36:12 +0200

    set all sign up pages

--- client/src/containers/signin/index.js
+++ client/src/containers/signin/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../../components/layout'
 import { Form, Button, Container, Row, Col } from 'react-bootstrap'
+import Input from '../../components/UI/Input'
 
 const Signin = () => {
   return (
@@ -9,18 +10,19 @@ const Signin = () => {
         <Row style={{ margin: '50px' }}>
           <Col md={{ span: 6, offset: 3 }}>
             <Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+              <Input
+                placeholder="Email"
+                label="Email"
+                type="email"
+                onChange={() => {}}
+              />
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
+              <Input
+                placeholder="Password"
+                label="Password"
+                type="password"
+                onChange={() => {}}
+              />
               <Button variant="primary" type="submit">
                 Submit
               </Button>
--- client/src/containers/signup/index.js
+++ client/src/containers/signup/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../../components/layout'
 
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import Input from '../../components/UI/Input'
 
 const Signup = () => {
   return (
@@ -11,31 +12,36 @@ const Signup = () => {
           <Col md={{ span: 6, offset: 3 }}>
             <Form>
               <Row>
-                <Col md = {6}>
-                  <Form.Group controlId="formBasicPassword">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control type="name" placeholder="FirstName" />
-                  </Form.Group>
+                <Col md={6}>
+                  <Input
+                    placeholder="First Name"
+                    label="First Name"
+                    type="text"
+                    onChange={() => {}}
+                  />
                 </Col>
-                <Col md = {6}>
-                  <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="name" placeholder="FirstName" />
-                  </Form.Group>
+                <Col md={6}>
+                  <Input
+                    placeholder="Last Name"
+                    label="Last Name"
+                    type="text"
+                    onChange={() => {}}
+                  />
                 </Col>
               </Row>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+              <Input
+                placeholder="Email"
+                label="Email"
+                type="email"
+                onChange={() => {}}
+              />
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
+              <Input
+                placeholder="Password"
+                label="Password"
+                type="password"
+                onChange={() => {}}
+              />
               <Button variant="primary" type="submit">
                 Submit
               </Button>
